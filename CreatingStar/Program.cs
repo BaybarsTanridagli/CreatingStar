c72dc3a66a87d6e080df063432adfc7132e1043c
Baybars Bozok Tanridagli <baybarsstanridagli@gmail.com>
2023-06-06 23:24:59 +0300

    CreatinStar

---
+++ CreatingStar/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace CreatingStar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Creating Star");
+            Console.WriteLine("Enter Value");
+            int value = Convert.ToInt32(Console.ReadLine());
+            
+            for (int i = 0; i < value; i++)
+            {
+                for (int k = value; k >= i; k--)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+
+            }
+            for (int i = (value-1); i> 1; i--)
+            {
+                for (int k = value; k >= i; k--)
+                {
+                    Console.Write(" ");
+                }
+                for (int j = 0; j < i; j++)
+                {
+                    Console.Write("* ");
+                }
+                Console.WriteLine();
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
+
